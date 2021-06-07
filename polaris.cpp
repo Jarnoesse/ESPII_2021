5fd3db7bea758ef40d1f10162aa51042c1c0c136
Rensach25 <84273558+Rensach25@users.noreply.github.com>
2021-06-07 16:20:17 +0200

    messo nuovi dati per la legge di Malus
    il fit è una meraviglia

--- polaris.cpp
+++ polaris.cpp
@@ -82,8 +82,8 @@ void polaris(){
   double alfamean_err[ns] = {};
   double prs[ns] = {-92.00, 66.37, 66.37}; // [deg*cm^3/(dm*g)] poteri rotatori specifici
   double prs_err = 0.01;
-  double l[] = {2., 2., 1.}; // [dm] lunghezza tubo porta compione DA CHIEDERE AL PROF!!!
-  double lerr[] = {1e-2, 1e-2, 1e-2,}; // DA CHIEDERE AL PROF!!!
+  double l[] = {2., 2., 1.}; // [dm] lunghezza tubo porta compione
+  double lerr[] = {1e-2, 1e-2, 1e-2,};
   double rho[ns] = {}; // [g/cm^3] concentrazione sostanze nelle provette
   double rhoerr[ns]= {};
   for (int i = 0; i < ns; ++i){
@@ -96,7 +96,8 @@ void polaris(){
 
     //PRINT
     cout << "Campione: " << sostanza[i]
-         << " \t Concentrazione: " << rho[i] << " +- " << rhoerr[i] << ") g/cm^3" << endl;
+         << " \t alfa: (" << alfamean[i] << " +- " << alfamean_err[i] << ") deg"
+         << " \t Concentrazione: (" << rho[i] << " +- " << rhoerr[i] << ") g/cm^3" << endl;
   }
 
   //------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -104,19 +105,22 @@ void polaris(){
   cout << "\n2. LEGGE di MALUS\n";
 
   //const int N = 37;
-  const int N = 36;
-  //const int N = 35;
+  //const int N = 36;
+  //const int N = 35-2; // ho tolto i punti strani
+  const int N = 37;
 
   double deg[N] = {}; // [rad]
   double degerr[N] = {};
 
   //elenco dei set di misure di I
   //double I[] = {5.7, 8.3, 10.9, 13.8, 16.5, 18.5, 19.9, 19.7, 18.4, 16.5, 13.8, 11.1, 8.4, 5.8, 4.1, 3., 2.6, 3.8, 6., 8.8, 12.4, 15.2, 17.7, 19.8, 20.8, 20.7, 19.4, 17.4, 14.5, 11.4, 8.3, 5.6, 3.6, 2.6, 2.8, 3.9, 5.8}; // [uA] corrente da fotodiodo
-  double I[] = {10.3, 9.6, 9.4, 9.3, 9.8, 10.4, 11.1, 11.9, 12.6, 13.2, 13.5, 13.7, 13.7, 13.4, 12.8, 12., 11.3, 10.5, 9.9, 9.5, 9.3, 9.4, 9.8, 10.4, 11.1, 11.9, 12.6, 13.2, 13.6, 13.7, 13.6, 13.1, 12.6, 11.9, 11.1, 10.7}; // [uA] set2
-  //double I[] = {5.1, 4.5, 3.9, 3.4, 3.1, 3., 3., 3.4, 3.9, 4., 4., 5., 5.6, 6.1, 6.4, 6.5, 6.7, 6.4, 5.8, 5.3, 4.8, 4.2, 3.6, 3.3, 3.3, 3.6, 4.1, 4.7, 5.1, 5.5, 5.9, 6.1, 6.3, 6.4, 5.8, 5.6}; // [uA] set3 dovrebbe essere quello senza sistematico ma le misure sono incomplete
+  //double I[] = {10.3, 9.6, 9.4, 9.3, 9.8, 10.4, 11.1, 11.9, 12.6, 13.2, 13.5, 13.7, 13.7, 13.4, 12.8, 12., 11.3, 10.5, 9.9, 9.5, 9.3, 9.4, 9.8, 10.4, 11.1, 11.9, 12.6, 13.2, 13.6, 13.7, 13.6, 13.1, 12.6, 11.9, 11.1, 10.7}; // [uA] set2
+  //double I[] = {5.1, 4.5, 3.9, 3.4, 3.1, 3., 3., 3.4, 3.9, 4., /*4. proviamo 4.5,*/ 5., 5.6, 6.1, 6.4, 6.5, 6.7, 6.4, 5.8, 5.3, 4.8, 4.2, 3.6, 3.3, 3.3, 3.6, 4.1, 4.7, 5.1, 5.5, 5.9, 6.1, 6.3, 6.4, 5.8/*, 5.6*/}; // [uA] set3 dovrebbe essere quello senza sistematico ma le misure sono incomplete
+  double I[] = {7.3, 11.5 ,15.9, 19.7, 23.5, 26.3, 27.8, 27.6, 26.3, 23.7, 20.1, 15.7, 11.4, 7.6, 4.6, 3.1, 3., 4.7, 7.6, 11.8, 16., 20.3, 23.8, 26.3, 27.6, 27.5, 26.1, 23.3, 19.6, 15.5, 11., 7.3, 4.4, 3., 2.8, 4.6, 7.6};
 
   double Ierr[N] = {};
   for (int i = 0; i < N; ++i){
+    //if (i==11) ++i;
     deg[i] = degtorad(10. * i);
     degerr[i] = degtorad(1.); // con 2. probfit quadra
     Ierr[i] = 1.5e-2 * I[i] + 0.1; // con 2. o 2.5 probfit quadra
@@ -136,7 +140,7 @@ void polaris(){
   gIdeg->Draw("AP");
 
   //FIT I = I0 * cos^2(theta)
-  TF1* Ideg = new TF1("Ideg", "[0] * pow(cos(x + [1]), 2) + [2]", min(N, deg)-0.15, max(N, deg));
+  TF1* Ideg = new TF1("Ideg", "[0] * pow(cos(x + [1]), 2) + [2]", min(N, deg)-0.15, max(N, deg)+ 0.015);
   Ideg->SetParameter(0, max(N, I));
   Ideg->SetParameter(1, 0);
   Ideg->SetParameter(2, 0);
@@ -146,8 +150,9 @@ void polaris(){
   Ideg->SetLineColor(2);
   gIdeg->Fit(Ideg, "RMS+");
   //cIdeg->Print("legge_di_Malus_SET1.png");
-  cIdeg->Print("legge_di_Malus_SET2.png");
+  //cIdeg->Print("legge_di_Malus_SET2.png");
   //cIdeg->Print("legge_di_Malus_SET3.png");
+  cIdeg->Print("legge_di_Malus_SET4.png");
 
   cout << "Chi^2: " << Ideg->GetChisquare() << ", number of DoF: " << Ideg->GetNDF() << " (Probability: " << Ideg->GetProb() << ").\n" << endl;
 }
